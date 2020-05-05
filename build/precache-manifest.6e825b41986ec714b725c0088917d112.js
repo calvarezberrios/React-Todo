d93df49fa14484a828f601ddee68263b6b9e9430
Carlos E. Alvarez-Berrios <calvarezberrios@Carloss-MacBook-Pro.local>
2020-05-05 15:04:03 -0400

    Created Build Version

---
+++ build/precache-manifest.6e825b41986ec714b725c0088917d112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3762b2a7d9f97da7f46cb5b893b7d491",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03cedf154c8fe8f767fc",
+    "url": "/static/css/main.f1571d1e.chunk.css"
+  },
+  {
+    "revision": "c72b611438e03f327385",
+    "url": "/static/js/2.04068deb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.04068deb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03cedf154c8fe8f767fc",
+    "url": "/static/js/main.88aafb38.chunk.js"
+  },
+  {
+    "revision": "4a0ff93597adaeb05a92",
+    "url": "/static/js/runtime-main.e9b6852c.js"
+  }
+]);
